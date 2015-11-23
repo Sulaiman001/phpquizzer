a61e60d238098c1cb6ff88b92a369a8296bc9457
Sal Allegra <salallegra@gmail.com>
2015-11-22 21:11:12 -0500

    Initial Commit

---
+++ Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Quizzer</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class = "container">
+		<h1>Quizzer</h1>
+		</div>
+
+	</header>
+
+</body>
+</html>
