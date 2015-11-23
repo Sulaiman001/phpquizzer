21af8b4ac54d5498fbf2c37d74700e8f3091a5d5
Sal Allegra <salallegra@gmail.com>
2015-11-22 21:49:42 -0500

    Finished fleshing out the simple ui of index.php

--- Index.php
+++ Index.php
@@ -13,9 +13,22 @@
 	</header>
 	<main>
 	<div class = "container">
-
-	</div>
 		<h2>Test Your PHP Knowledge</h2>
+		<p>This is a multiple choice quiz to test your knowledge of php</p>
+		<ul>
+			<li>
+				<strong>Number of Questions:</strong>
+			</li>
+			<li>
+				<strong>Type:</strong>
+			</li>
+			<li>
+				<strong>Estimated Time:</strong>
+			</li>
+		</ul>
+		<a href = "question.php?n=1" class = "start">Start</a>
+	</div>
+		
 	</main>
 	<footer>
 		<div class = "container">
