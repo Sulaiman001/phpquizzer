506713cf1abcb3c69448cab7026ed8df1b821c05
Sal Allegra <salallegra@gmail.com>
2015-11-22 21:53:01 -0500

    Added question.php to process questions and copied over markup from index.php

---
+++ question.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Question</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class = "container">
+		<h1>Quizzer</h1>
+		</div>
+
+	</header>
+	<main>
+	<div class = "container">
+		
+	</div>
+		
+	</main>
+	<footer>
+		<div class = "container">
+		Copywrite 2015
+		</div>
+</body>
+</html>
