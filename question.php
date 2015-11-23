730cfea2d6a6a8e6627438e62b3c1e5d5798a7dd
Sal Allegra <salallegra@gmail.com>
2015-11-23 16:35:18 -0500

    added question.php to project

--- question.php
+++ question.php
@@ -7,19 +7,31 @@
 <body>
 	<header>
 		<div class = "container">
-		<h1>Quizzer</h1>
+			<h1>Quizzer</h1>
 		</div>
 
 	</header>
 	<main>
-	<div class = "container">
-		
-	</div>
+		<div class = "container">
+			<div class = "current">Question 1 of 5</div>
+			<p class = "question">
+				What does php stand for?
+			</p>
+			<form method = "post" action = "process.php">
+				<ul class = "choices">
+					<li><input name = "choice" type = "radio" value = "1"</li>PHP: Hypertext Preprocessor</li>
+					<li><input name = "choice" type = "radio" value = "1"</li>PHP: Hypertext Preprocessor</li>
+					<li><input name = "choice" type = "radio" value = "1"</li>PHP: Hypertext Preprocessor</li>
+
+				</ul>
+				<input type = "submit" value = "submit">
+			</form>
+		</div>
 		
 	</main>
 	<footer>
 		<div class = "container">
-		Copywrite 2015
+			Copywrite 2015
 		</div>
-</body>
-</html>
+	</body>
+	</html>
