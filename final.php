f48b7ec91fa05cf45b6a7ee78b0e37554114d492
Sal Allegra <salallegra@gmail.com>
2015-11-23 17:51:54 -0500

    Added final.php to the project, this will be the view the user sees when the quiz is over

--- final.php
+++ final.php
@@ -13,6 +13,10 @@
 </header>
 <main>
 	<div class = "container">
+	<h2>You're Done!</h2>
+	<p>Congrat! You've completed the test</p>
+	<p>Final Score: 5</p>
+	<a href = "question.php?n=1" class = "start">Take Again</a>
 		
 	</div>
 	
