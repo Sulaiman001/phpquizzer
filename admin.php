f14e32baee9c8945472a60f40151b0b2f58ff5e6
Sal Allegra <salallegra@gmail.com>
2015-11-23 17:45:24 -0500

    Added admin.php, admin.php will allow a quiz administrator to add a new question to the quiz bank with 5 possible answers and also add what index the question will be in the total amount of questions

---
+++ admin.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Admin - Add/Delete Questions</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<header>
+		<div class = "container">
+			<h1>PHP Quizzer</h1>
+		</div>
+
+	</header>
+	<main>
+		<div class = "container">
+			<h2>Add a Question</h2>
+			<form method = "post" action="add.php">
+				<p>					
+					<label>Question Number</label>
+					<input type = "number" name = "question_number"/>
+				</p>
+				<p>					
+					<label>Question Text</label>
+					<input type = "text" name = "question_text"/>
+				</p>
+				<p>					
+					<label>Choice #1</label>
+					<input type = "text" name = "choice1"/>
+				</p>
+				<p>					
+					<label>Choice #2</label>
+					<input type = "text" name = "choice2"/>
+				</p>
+				<p>					
+					<label>Choice #3</label>
+					<input type = "text" name = "choice3"/>
+				</p>
+				<p>					
+					<label>Choice #4</label>
+					<input type = "text" name = "choice4"/>
+				</p>
+				<p>					
+					<label>Choice #5</label>
+					<input type = "text" name = "choice4"/>
+				</p>
+			</form>
+		
+	</main>
+	<footer>
+		<div class = "container">
+			Copywrite 2015
+		</div>
+	</body>
+	</html>
