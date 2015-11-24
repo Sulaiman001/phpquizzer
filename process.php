304042d657f7f1c5212e99b3872f9cba99cddd12
Sal Allegra <salallegra@gmail.com>
2015-11-24 16:49:22 -0500

    Added logic to check if the answer is correct, and to redirect to next question or final page

--- process.php
+++ process.php
@@ -11,6 +11,40 @@
 	if($_POST){
 		$number = $_POST['number'];
 		$selected_choice = $_POST['choice'];
-		echo $number.'<br />';
-		echo $selected_choice;
+		$next = $number+1;
+
+		//Get total amount of questions
+		$query = "SELECT * FROM `questions`";
+		//Get result
+		$results = $mysqli->query($query) or die($mysqli->error._LINE_);
+		$total = $results->num_rows;
+
+
+
+		// Get Correct choice
+
+		$query = "SELECT * FROM `choices`
+		WHERE question_number = $number and is_correct = 1";
+
+		//get the result
+		$result = $mysqli->query($query) or die($mysqli->error._LINE_);
+
+		//Get row
+		$row = $result->fetch_assoc();
+		$correct_choice = $row['id'];
+
+		//Compare
+		if($correct_choice == $selected_choice){
+			//Answer is correct
+			$_SESSION['score']++;
+
+		}
+		if($number == $total){
+			header("Location: final.php");
+			exit();
+
+		} else {
+			header("Location: question.php?n=".$next);
+		}
+
 }
