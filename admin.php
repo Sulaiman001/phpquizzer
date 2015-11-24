6ac49d0bde65e8068b723633d2c70625d87c1379
Sal Allegra <salallegra@gmail.com>
2015-11-24 18:55:16 -0500

    Added sql queries to insert a question and 5 choices to the database, I also set the question number to one more than the number amount of questions in the database

--- admin.php
+++ admin.php
@@ -1,84 +1,126 @@
 <?php include 'database.php'; ?>
-
-
-<?php 
-	if (isset($_POST['submit'])){
-		echo 'Form was submitted';
+<?php
+	if(isset($_POST['submit'])){
 		//Get post variables
 		$question_number = $_POST['question_number'];
 		$question_text = $_POST['question_text'];
-
-		//creeate an array of choices for the form
-
+		$correct_choice = $_POST['correct_choice'];
+		//Choices array
 		$choices = array();
 		$choices[1] = $_POST['choice1'];
 		$choices[2] = $_POST['choice2'];
 		$choices[3] = $_POST['choice3'];
 		$choices[4] = $_POST['choice4'];
 		$choices[5] = $_POST['choice5'];
-
-
+		
+		//Question query
+		$query = "INSERT INTO `questions`(question_number, text)
+					VALUES('$question_number','$question_text')";
+					
+		//Run query
+		$insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+		
+		//Validate insert
+		if($insert_row){
+			foreach($choices as $choice => $value){
+				if($value != ''){
+					if($correct_choice == $choice){
+						$is_correct = 1;
+					} else {
+						$is_correct = 0;
+					}
+					//Choice query
+					$query = "INSERT INTO `choices` (question_number, is_correct, text)
+							VALUES ('$question_number','$is_correct','$value')";
+							
+					//Run query
+					$insert_row = $mysqli->query($query) or die($mysqli->error.__LINE__);
+					
+					//Validate insert
+					if($insert_row){
+						continue;
+					} else {
+						die('Error : ('.$mysqli->errno . ') '. $mysqli->error);
+					}
+				}
+			}
+			$msg = 'Question has been added';
+		}
 	}
-
-
- ?> 
-
+	
+	/*
+ 	* Get total questions
+	*/
+	$query = "SELECT * FROM `questions`";
+	//Get The Results
+	$questions = $mysqli->query($query) or die($mysqli->error.__LINE__);
+	$total = $questions->num_rows;
+	$next = $total+1;
+?>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Admin - Add/Delete Questions</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<head>
+	<meta charset="utf-8" />
+	<title>PHP Quizzer</title>
+	<link rel="stylesheet" href="css/style.css" type="text/css" />
 </head>
 <body>
 	<header>
-		<div class = "container">
+		<div class="container">
 			<h1>PHP Quizzer</h1>
 		</div>
-
 	</header>
 	<main>
-		<div class = "container">
-			<h2>Add a Question</h2>
-			<form method = "post" action="admin.php">
-				<p>					
-					<label>Question Number</label>
-					<input type = "number" name = "question_number"/>
+		<div class="container">
+			<h2>Add A Question</h1>
+			<?php
+				if(isset($msg)){
+					echo '<p>'.$msg.'</p>';
+				}
+			?>
+			<form method="post" action="admin.php">
+				<p>
+					<label>Question Number: </label>
+					<input type="number" value="<?php echo $next; ?>" name="question_number" />
 				</p>
-				<p>					
-					<label>Question Text</label>
-					<input type = "text" name = "question_text"/>
+				<p>
+					<label>Question Text: </label>
+					<input type="text" name="question_text" />
 				</p>
-				<p>					
-					<label>Choice #1</label>
-					<input type = "text" name = "choice1"/>
+				<p>
+					<label>Choice #1: </label>
+					<input type="text" name="choice1" />
 				</p>
-				<p>					
-					<label>Choice #2</label>
-					<input type = "text" name = "choice2"/>
+				<p>
+					<label>Choice #2: </label>
+					<input type="text" name="choice2" />
 				</p>
-				<p>					
-					<label>Choice #3</label>
-					<input type = "text" name = "choice3"/>
+				<p>
+					<label>Choice #3: </label>
+					<input type="text" name="choice3" />
 				</p>
-				<p>					
-					<label>Choice #4</label>
-					<input type = "text" name = "choice4"/>
+				<p>
+					<label>Choice #4: </label>
+					<input type="text" name="choice4" />
 				</p>
-				<p>					
-					<label>Choice #5</label>
-					<input type = "text" name = "choice5"/>
+				<p>
+					<label>Choice #5: </label>
+					<input type="text" name="choice5" />
 				</p>
-				<p>					
-					<label>Correct choice #</label>
-					<input type = "number" name = "correct_choice"/>
+				<p>
+					<label>Correct Choice Number: </label>
+					<input type="number" name="correct_choice" />
 				</p>
-				<input type = "submit" name = "submit" value = "Add Question" class = "" />
+				<p>
+					<input type="submit" name="submit" value="Submit" />
+				</p>
 			</form>
-		
+		</div>
 	</main>
 	<footer>
-		<div class = "container">
-			Copywrite 2015
+		<div class="container">
+			Copyright &copy; 2014, PHP Quizzer
 		</div>
-	</body>
-	</html>
+	</footer>
+</body>
+</html>
