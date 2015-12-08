9da60836cb54b6248933c128595e63cff9f1768c
Sal Allegra <salallegra@gmail.com>
2015-12-08 15:26:25 -0500

    Changed indentation

--- question.php
+++ question.php
@@ -6,7 +6,6 @@
 	if($number == 1){
 		$_SESSION['score'] = 0;
 	}
-
 	/*
 	*	Get Question
 	*/
@@ -15,8 +14,7 @@
 	//Get result
 	$result = $mysqli->query($query) or die($mysqli->error.__LINE__);
 	
-	$question = $result->fetch_assoc();
-	
+	$question = $result->fetch_assoc();	
 	/*
 	*	Get Choices
 	*/
@@ -30,32 +28,30 @@
 		//Get result
 		$results = $mysqli->query($query) or die($mysqli->error._LINE_);
 		$total = $results->num_rows;
-
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Question</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+  <title>Question</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
-	<header>
-		<div class = "container">
-			<h1>Quizzer</h1>
-		</div>
-
-	</header>
-	<main>
-		<div class = "container">
-			<div class = "current">Question <?php echo $question['question_number']; ?> of <?php echo $total; ?></div>
-			<p class = "question">
-				<?php echo $question['text']; ?>
-			</p>
+  <header>
+    <div class = "container">
+      <h1>Quizzer</h1>
+	</div>
+  </header>
+<main>
+<div class = "container">
+  <div class = "current">Question <?php echo $question['question_number']; ?> of <?php echo $total; ?></div>
+    <p class = "question">
+	  <?php echo $question['text']; ?>
+	</p>
 			<form method = "post" action = "process.php">
 				<ul class = "choices">
 				<?php while($row = $choices->fetch_assoc()) : ?>
 					<li><input name = "choice" type = "radio" value = "<?php echo $row['id'] ?>"</li><?php echo $row['text'] ?></li>
-					<?php endwhile; ?>
+				<?php endwhile; ?>
 				</ul>
 				<input type = "submit" value = "submit">
 				<input type = "hidden" name = "number" value = "<?php echo $number; ?>" />
